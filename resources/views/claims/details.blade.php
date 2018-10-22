7e614bfbd295b5d296ea0cc19a329529ef32420b
Fazal-E- Rabbi <fer@ciklum.com>
2018-10-22 18:31:56 +0500

    Rotation

--- resources/views/claims/details.blade.php
+++ resources/views/claims/details.blade.php
@@ -209,8 +209,8 @@
     @if(count($claim->images) > 0)
         <div class="row">
             @foreach($claim->images as $key => $image)
-                <div class="column">
-                    <img src="{{ asset('/images/'.$image->image) }}" style="width:100%; max-height: 150px;min-height: 150px;" onclick="openModal();currentSlide({{ $key }})" class="">
+                <div class="">
+                    <img src="{{ asset('/images/'.$image->image) }}" style="width:100%; max-height: 150px;min-height: 150px;" onclick="openModal();currentSlide({{ $key }})" class="hover-shadow cursor">
                 </div>
             @endforeach
         </div>
