f2fce7c3a33de39bbb326223f0e902c978827c3a
Fazal-E- Rabbi <fer@ciklum.com>
2018-11-05 15:02:40 +0500

    done

--- app/Mail/SendEmailToCustomerEmail.php
+++ app/Mail/SendEmailToCustomerEmail.php
@@ -48,14 +48,18 @@ class SendEmailToCustomerEmail extends Mailable
             $subject = $this->customer->name;
         }
         if($this->claim->address1) {
-            $subject = $subject . ', afd. '.$this->claim->address1->address;
+            $subject = $subject . ', Skadested. '.$this->claim->address1->address;
+        }
+        if(!empty($this->claim->address_2)) {
+            $subject = $subject . ' - Nr./etage/side, '.$this->claim->address_2;
         }
         if(!empty($this->claim->customer_policy_number)) {
             $subject = $subject . ' - police nr,: '.$this->claim->customer_policy_number;
         }
-        if(!empty($this->claim->address_2)) {
-            $subject = $subject . ' - '.$this->claim->address_2;
+        if(!empty($this->claim->selsskab_skade_nummer)) {
+            $subject = $subject . ' - selskab skade nr,: '.$this->claim->selsskab_skade_nummer;
         }
+
         return $this->from('no_reply@bnk.com')
                     ->subject($subject)
                     ->with(
