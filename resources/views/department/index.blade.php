0d4d7bcf4e36dc1678499704579549c970b8bb98
Fazal-E- Rabbi <fer@ciklum.com>
2019-02-18 14:24:32 +0500

    Messages

--- resources/views/department/index.blade.php
+++ resources/views/department/index.blade.php
@@ -68,7 +68,7 @@
                         @foreach($departments as $department)
                                 @foreach($department->addresses as $key => $address)
                                     <tr id="content_{{ $department->id }}">
-                                        <td>{{ $department->name }}</td>
+                                        <td data-order="{{ intval($department->name) }}">{{ $department->name }}</td>
                                         <td style="width: 150px;" class="col-md-3 col-xs-12" >{{ $address->address}}</td>
                                         <td style="width: 150px;" class="col-md-3 col-xs-12" >{{ $address->zip_code}}</td>
                                         <td style="width: 150px;" class="col-md-3 col-xs-12" >{{ $address->city}}</td>
