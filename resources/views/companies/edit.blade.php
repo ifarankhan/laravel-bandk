687d0ccc01b2772beb6bb954518910a9694ab67d
Fazal-E- Rabbi <fer@ciklum.com>
2019-09-16 13:54:36 +0500

    Changes

--- resources/views/companies/edit.blade.php
+++ resources/views/companies/edit.blade.php
@@ -9,7 +9,7 @@
         <div class="col-md-12 col-sm-12 col-xs-12">
             <div class="x_panel">
                 <div class="x_title">
-                    <h2>Edit Customer </h2>
+                    <h2>Edit Company </h2>
                     <div class="clearfix"></div>
                 </div>
                 <div class="x_content">
