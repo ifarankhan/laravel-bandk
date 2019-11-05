170e084793cf21dca8107e42a34bdafa2587b72b
Fazal-E- Rabbi <fer@ciklum.com>
2019-11-05 16:26:23 +0500

    Grouped data on basis of customer

--- resources/views/department/index.blade.php
+++ resources/views/department/index.blade.php
@@ -55,7 +55,7 @@
                     </form>
                     <?php
                     $departmentsArray = [];
-                    if($search && isset($search['customer_id']) && count($departments) > 0) {
+                    if(count($departments) > 0) {
                         foreach ($departments as $department) {
                             $departmentsArray[$department->id][] = $department;
                         }
