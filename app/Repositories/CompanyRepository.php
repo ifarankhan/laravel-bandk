d3f0a86fd033bba540013690bae2ce8debccb6bc
Fazal-E- Rabbi <fer@ciklum.com>
2020-01-23 15:51:34 +0500

    Changes

--- app/Repositories/CompanyRepository.php
+++ app/Repositories/CompanyRepository.php
@@ -62,9 +62,9 @@ class CompanyRepository implements CompanyInterface
                     }
                     usort($userDepartmentsArray, 'sortMyArray');
 
-                   /* foreach ($userDepartmentsArray as $key => $dep) {
-                        $userDepartmentsArray[$key]->order = $key + 1;
-                    }*/
+                    foreach ($userDepartmentsArray as $d => $dep) {
+                        $userDepartmentsArray[$d]->order = $d + 1;
+                    }
                     $collect = collect($userDepartmentsArray);
 
                     unset($companies[$key]->departments);
